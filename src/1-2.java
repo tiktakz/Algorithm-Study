3a0cdbe2eaa31f542922ff7191b48cb37a229d3d
tiktakz <whitehouseon@naver.com>
2022-10-16 23:07:46 +0900

    복습

---
+++ src/1-2.java
@@ -0,0 +1,25 @@
+// review
+
+import java.util.Scanner;
+
+class Main1_2{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(solution(str));
+    }
+
+    public static String solution(String str) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : str.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                sb.append(Character.toLowerCase(c));
+            }
+            else {
+                sb.append(Character.toUpperCase(c));
+            }
+        }
+        String answer = sb.toString();
+        return answer;
+    }
+}
---
+++ src/1-3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+//review
+class Main1_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(solution(str));
+    }
+
+    public static String solution(String str) {
+        String[] strArr = str.split(" ");
+        String answer = strArr[0];
+        int max = strArr[0].length();
+        for (String s : strArr) {
+            if (s.length() > max) {
+                max = s.length();
+                answer = s;
+            }
+        }
+        return answer;
+    }
+}
