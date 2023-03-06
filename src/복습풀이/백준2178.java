08be243d1b5bfd03d18a3aec4b2470aa749f8b6d
KimJungWoo <86278689+tiktakz@users.noreply.github.com>
2023-03-06 16:31:33 +0900

    Update 백준2178.java

--- src/복습풀이/백준2178.java
+++ src/복습풀이/백준2178.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Main {
     static class Node {
     int x, y;
-    public Node1 (int x, int y) {
+    public Node(int x, int y) {
         this.x = x;
         this.y = y;
        }
